6df4b38ddc99b637bb57212eac84f9b0401eb40f
Pierre Cavalet <pcavalet@kaliop.com>
2023-06-14 15:22:57 +0200

    go

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <h1>Pokemon list</h1>
+    <h1>{{ title }}</h1>
+    <button @click="clickMe">click me</button>
     <ul>
       <li v-for="pokemon in pokemons" :key="pokemon.id">
         <NuxtLink :to="`/pokemon/${pokemon.id}`">
@@ -15,4 +16,10 @@
 const { data: pokemons } = await useAsyncData("pokemons", () =>
   $fetch("/api/pokemon")
 );
+
+const title = ref("Pokemon list");
+
+const clickMe = () => {
+  title.value = "I CHANGED MY TITLE YESSS";
+};
 </script>
